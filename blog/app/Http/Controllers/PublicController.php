00606e81f7c3b47558cfaa7c7c053bd7049565c8
daniel_janeski <danyyoh@gmail.com>
2017-04-24 21:02:01 +0200

    Laravel

---
+++ blog/app/Http/Controllers/PublicController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PublicController extends Controller
+{
+    public function about () {
+        return view('about', [
+        
+        'name' => 'SEDC',
+        'team' => [
+            'John Doe',
+            'Jane Doe'
+        ]
+        ]);
+    }
+}
