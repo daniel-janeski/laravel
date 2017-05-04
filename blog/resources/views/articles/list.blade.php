5f3671d6f6c6eedd3a7bcffcfc46c413a2f0ecb9
daniel_janeski <danyyoh@gmail.com>
2017-05-04 21:06:16 +0200

    Laravel Articles

---
+++ blog/resources/views/articles/list.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts/layout')
+
+@section('content')
+<h1 class="page-header">Articles <a href="{{ url('articles/create') }}"><i class="fa fa-plus" aria-hidden="true"></i>
+</a></h1>
+@foreach ($articles as $article) 
+<article>
+    <h2><a href='#'>{{ $article->title }}</a> <a href="{{ url('articles/edit', ['id'=>$article->id]) }}"><i class="fa fa-pencil" aria-hidden="true"></i>
+</a></h2>
+    <div class='content'>{{ $article->excerpt }}</div>
+</article>
+@endforeach
+@endsection
